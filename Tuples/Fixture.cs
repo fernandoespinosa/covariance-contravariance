be4eb711b98b772990ee5368b3ac251e0fe01cd7
Fernando Espinosa <fespinosa@loyalty.com>
2019-02-28 11:49:00 -0500

    TEMP

--- Tuples/Fixture.cs
+++ Tuples/Fixture.cs
@@ -1,13 +1,12 @@
 
 using System;
+using System.IO;
 using NUnit.Framework;
 
 namespace GameOfCodes.Tuples
 {
-
     public class Tuples
     {
-
         [Test]
         public void Tuples_Test()
         {
@@ -15,6 +14,16 @@ namespace GameOfCodes.Tuples
             Console.WriteLine(namedLetters);
             Console.WriteLine(namedLetters.a);
             Console.WriteLine(namedLetters.b);
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Foo_Test()
+        {
+            FileInfo fileInfo = new FileInfo("~/Spikes/game-of-codes/SubsequenceFinder.Tests.cs");
+            Console.WriteLine($"Full Name: {fileInfo.FullName}");
+            Console.WriteLine(fileInfo.Exists);
+            Console.Write(File.Exists("~/Spikes/game-of-codes/SubsequenceFinder.Tests.cs"));
             Assert.Pass();
         }
     }
